e90cd50b6370947a92f4b78946141fd5b82e4493
gbs4ever <gbs4ever2@gmail.com>
2019-04-30 22:31:45 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
+let eastWest = [
+  '1st Avenue',
+  '2nd Avenue',
+  '3rd Avenue',
+  'Lexington Avenue',
+  'Park',
+  'Madison Avenue',
+  '5th Avenue'
+];
+
+
+class Driver{
+  constructor(name,startDate){  
+    this.name = name
+    this.startDate  = new Date(startDate )
+  }
+  yearsExperienceFromBeginningOf(year) {
+   const  startYear = this.startDate
+    const endYear = new Date (year,0) 
+    return endYear.getFullYear() - startYear.getFullYear()
+  
+  }
+ 
+
+  
+
+}
+class Route{
+  constructor(beginningLocation,endingLocation){
+  this.beginningLocation = beginningLocation 
+  this.endingLocation = endingLocation
+  }
+  blocksTravelled(){
+
+  }
+
+}
+
+
+
 
